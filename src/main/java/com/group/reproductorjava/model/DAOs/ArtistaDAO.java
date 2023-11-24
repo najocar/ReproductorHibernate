c57d92ec4519f4bc5e3e50e93438044c58307aea
Jose Javier Galan <josejaviergalan2003@gmail.com>
2023-11-24 16:09:41 +0100

    ArtistaDAO modificado

--- src/main/java/com/group/reproductorjava/model/DAOs/ArtistaDAO.java
+++ src/main/java/com/group/reproductorjava/model/DAOs/ArtistaDAO.java
@@ -1,242 +1,95 @@
 package com.group.reproductorjava.model.DAOs;
 
-import java.sql.*;
-import java.util.ArrayList;
+import com.group.reproductorjava.model.Entity.Artista;
+import com.group.reproductorjava.model.Entity.Disco;
+import org.hibernate.Transaction;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
-import com.group.reproductorjava.model.Entity.Disco;
-import com.group.reproductorjava.model.Connection.MariaDBConnection;
-import com.group.reproductorjava.model.Entity.Artista;
-import com.group.reproductorjava.model.interfaces.IArtistaDAO;
-import com.group.reproductorjava.utils.LoggerClass;
+public class ArtistaDAO {
 
-public class ArtistaDAO extends Artista implements IArtistaDAO {
+    private static final EntityManager EM = Manager.getEntityManager();
 
-
-    private final static String INSERT = "INSERT INTO artista (nombre, nacionalidad,foto) VALUES(?, ?, ?)";
-    private final static String UPDATE = "UPDATE artista SET nombre=?, foto=?, nacionalidad=? WHERE id=?";
-    private final static String DELETE = "DELETE FROM artista WHERE id=?";
-    private final static String SELECT_BY_ID = "SELECT * FROM artista WHERE id=?";
-    private final static String SELECT_ALL = "SELECT * FROM artista";
-    private final static String SELECT_BY_NAME = "SELECT * FROM artista WHERE nombre=?";
-
-    /**
-     *
-     */
-    static LoggerClass logger = new LoggerClass(ArtistaDAO.class.getName());
-
-    public ArtistaDAO(int id) {
-        getArtista(id);
-    }
-
-    public ArtistaDAO(Artista a){
-        super(a.getId(),a.getName(),a.getPhoto(),a.getNacionality());
-        this.discos=a.getDiscos();
-    }
-
-    public ArtistaDAO(int id, String name, String photo, String nacionality){
-        super(id,name,photo,nacionality);
-    }
-
-    public ArtistaDAO() {
-
-    }
-
-    /**
-     * Metodo save Artista que guarda un artista en la base de datos
-     * @return true si se guarda
-     * @return  false si no se guarda
-     */
-    public boolean saveArtista() {
-        if (getId() != -1) {
-            return updateArtista();
-        }else {
-            Connection conn = MariaDBConnection.getConnection();
-            if (conn == null)
-                return false;
-
-            try (PreparedStatement ps = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS)) {
-                ps.setString(1, getName());
-                ps.setString(2, getNacionality());
-                ps.setString(3, getPhoto());
-
-                if (ps.executeUpdate() == 1) {
-                    try (ResultSet rs = ps.getGeneratedKeys()) {
-                        if (rs.next()) {
-                            setId(rs.getInt(1));
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
-                }
-
-                return false;
-            } catch (SQLException e) {
-                logger.warning("Error to try save Artist");
-                logger.warning(e.getMessage());
-                return false;
+    public boolean saveArtista(Artista artista) {
+        Transaction transaction = null;
+        try {
+            transaction = EM.getTransaction();
+            transaction.begin();
+            EM.persist(artista);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
             }
-        }
-    }
-
-    /**
-     * Metodo updateArtista que actualiza los datos de un artista existente
-     * @return true si se hace correctamente
-     * @return false si hubo algun fallo
-     */
-    public boolean updateArtista() {
-        if(getId()==-1) return false;
-        Connection conn = MariaDBConnection.getConnection();
-        if (conn == null)
-            return false;
-        try (PreparedStatement ps = conn.prepareStatement(UPDATE)) {
-            ps.setString(1, getName());
-            ps.setString(2, getPhoto());
-            ps.setString(3, getNacionality());
-            ps.setInt(4, getId());
-            if(ps.executeUpdate()==1) return true;
-            setId(-1);
-            return false;
-        } catch (SQLException e) {
-            logger.warning("Error to try update Artist");
-            logger.warning(e.getMessage());
+            e.printStackTrace();
             return false;
         }
     }
 
-    /**
-     * Metodo delete artista que borra el artista de la base de datos
-     * @return true si se ha borrado
-     * @retrun false si no se ha podido borrar
-     */
-    public boolean deleteArtista() {
-        Connection conn = MariaDBConnection.getConnection();
-        if (conn == null)
-            return false;
-
-        try (PreparedStatement ps = conn.prepareStatement(DELETE)) {
-            ps.setInt(1, getId());
-
-            return ps.executeUpdate() == 1;
-        } catch (SQLException e) {
-            logger.warning("Error to try delete Artist");
-            logger.warning(e.getMessage());
-            return false;
-        }
-    }
-
-    /**
-     * Metodo get artista que busca el artista en la base de datos por el id
-     * @param id del artista a buscar
-     * @return true si se ha encontrado
-     * @return false si no se ha encontrado
-     */
-    public boolean getArtista(int id) {
-        Connection conn = MariaDBConnection.getConnection();
-        if (conn == null)
-            return false;
-
-        try (PreparedStatement ps = conn.prepareStatement(SELECT_BY_ID)) {
-            ps.setInt(1, id);
-            if (ps.execute()) {
-                try (ResultSet rs = ps.getResultSet()) {
-                   if(rs.next()){
-                       setId(rs.getInt("id"));
-                       setName(rs.getString("nombre"));
-                       setNacionality(rs.getString("nacionalidad"));
-                       setPhoto(rs.getString("foto"));
-                   }else{
-                       return false;
-                   }
-
-                }
+    public boolean updateArtista(Artista artista) {
+        Transaction transaction = null;
+        try {
+            transaction = EM.getTransaction();
+            transaction.begin();
+            EM.merge(artista);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
             }
-        } catch (SQLException e) {
-            logger.warning("Error to try get Artist by id");
-            logger.warning(e.getMessage());
+            e.printStackTrace();
             return false;
         }
-        return true;
     }
 
-    /**
-     * Metodo getArtista que busca al artista en la base de datos por el nombre del artista
-     * @param name del artista a buscar
-     * @return true si se ha encontrado
-     * @return false si no se ha encontrado
-     */
-    @Override
-    public boolean getArtista(String name) {
-        Connection conn = MariaDBConnection.getConnection();
-        if (conn == null)
-            return false;
-        try (PreparedStatement ps = conn.prepareStatement(SELECT_BY_NAME)) {
-            ps.setString(1, name);
-            if (ps.execute()) {
-                try (ResultSet rs = ps.getResultSet()) {
-                    if(rs.next()){
-                        setId(rs.getInt("id"));
-                        setName(rs.getString("nombre"));
-                        setNacionality(rs.getString("nacionalidad"));
-                        setPhoto(rs.getString("foto"));
-                    }else{
-                        return false;
-                    }
-                }
+    public boolean deleteArtista(int id) {
+        Transaction transaction = null;
+        try {
+            transaction = EM.getTransaction();
+            transaction.begin();
+            Artista artista = EM.find(Artista.class, id);
+            EM.remove(artista);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
             }
-        } catch (SQLException e) {
-            logger.warning("Error to try get Artist by name");
-            logger.warning(e.getMessage());
+            e.printStackTrace();
             return false;
         }
-        return true;
     }
 
-    /**
-     * metodo getallartist que obtienen todos los artistas de la base de datos
-     * @return List con todos los artistas de las base de datos
-     */
-    public List<Artista> getAllArtists() {
-        Connection conn = MariaDBConnection.getConnection();
-        if (conn == null)
-            return null;
+    public Artista getArtista(int id) {
+        return EM.find(Artista.class, id);
+    }
 
-        List<Artista> artistas = new ArrayList<>();
-
-        try (PreparedStatement ps = conn.prepareStatement(SELECT_ALL)) {
-            if (ps.execute()) {
-                try (ResultSet rs = ps.getResultSet()) {
-                    while (rs.next()) {
-                        Artista artista = new Artista(
-                                rs.getInt("id"),
-                                rs.getString("nombre"),
-                                rs.getString("nacionalidad"),
-                                rs.getString("foto")
-                        );
-                        artistas.add(artista);
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            logger.warning("Error to try update Artist");
-            logger.warning(e.getMessage());
+    public Artista getArtistaByName(String name) {
+        Query query = EM.createQuery("SELECT a FROM Artista a WHERE a.name = :name");
+        query.setParameter("name", name);
+        List<Artista> artistas = query.getResultList();
+        if (!artistas.isEmpty()) {
+            return artistas.get(0);
+        } else {
             return null;
         }
-
-        return artistas;
     }
 
-    /**
-     * metodo getdiscos que obtiene los discos que tengan el id del artista
-     * @return list con los discos
-     */
-   public List<Disco>getDiscos(){
-        if(super.getDiscos()==null){
-            setDiscos(DiscoDAO.getAllDiscosByArtista(this));
+    public List<Artista> getAllArtistas() {
+        Query query = EM.createQuery("SELECT a FROM Artista a");
+        return query.getResultList();
+    }
+
+    public List<Disco> getDiscos(int id) {
+        Artista artista = getArtista(id);
+        if (artista != null) {
+            return artista.getDiscos();
+        } else {
+            return null;
         }
-        return super.getDiscos();
-   }
-
-
+    }
 }
