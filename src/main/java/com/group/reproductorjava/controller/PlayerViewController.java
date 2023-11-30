1bf5e7fb234cff22c7153c61fe77c5ecf896813e
Jose Javier Galan <josejaviergalan2003@gmail.com>
2023-11-30 09:52:44 +0100

    playerview v3

--- src/main/java/com/group/reproductorjava/controller/PlayerViewController.java
+++ src/main/java/com/group/reproductorjava/controller/PlayerViewController.java
@@ -8,6 +8,7 @@ import com.group.reproductorjava.model.DTOs.ControlDTO;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -24,6 +25,9 @@ public class PlayerViewController {
 
     @FXML
     private Button play_btn;
+
+    @FXML
+    private ProgressBar pb;
 
     @FXML
     private Button stop_btn;
