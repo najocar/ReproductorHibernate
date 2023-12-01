8b20e5336fa8c09346e7b6fd223dd7d4869335f9
Ruben <r.c.a2312@gmail.com>
2023-12-01 11:23:59 +0100

    update RegisterView and RegisterViewController

--- src/main/java/com/group/reproductorjava/controller/RegisterViewController.java
+++ src/main/java/com/group/reproductorjava/controller/RegisterViewController.java
@@ -1,4 +1,39 @@
 package com.group.reproductorjava.controller;
 
-public class RegisterViewController {
+import com.group.reproductorjava.HelloApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterViewController implements Initializable {
+
+    @FXML
+    private TextField userField, passField;
+
+    @FXML
+    private Button register_btn, goLogin_btn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private void goLogin() {
+        try {
+            HelloApplication.setRoot("LoginView");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    private void register() {
+        return;
+    }
 }
