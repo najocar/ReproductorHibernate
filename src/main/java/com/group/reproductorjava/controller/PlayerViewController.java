4c835fee8b4f189a5a59ec857e75d454bd2dd934
Jose Javier Galan <josejaviergalan2003@gmail.com>
2023-12-03 20:15:32 +0100

    playerview fixed

--- src/main/java/com/group/reproductorjava/controller/PlayerViewController.java
+++ src/main/java/com/group/reproductorjava/controller/PlayerViewController.java
@@ -96,11 +96,11 @@ public class PlayerViewController {
         userlabel.setText(userDao.getName());
 
         String imagePath = userDao.getPhoto();
-//        if (imagePath != null) {
-//            Image imagenJ = new Image(new File("com/group/reproductorjava/images/" +imagePath).toURI().toString());
-//            image.setImage(imagenJ);
-//        }
-//        setSongName(ControlDTO.getUser().getName());
+     if (imagePath != null) {
+           Image imagenJ = new Image(new File("com/group/reproductorjava/images/" +imagePath).toURI().toString());
+           image.setImage(imagenJ);
+       }
+       setSongName(ControlDTO.getUser().getName());
     }
     /**
      * Establece el nombre de la canción seleccionada.
@@ -118,36 +118,36 @@ public class PlayerViewController {
     @FXML
     private void Play() {
         if (isPlaying) {
-//            pause();
+            pause();
             play_btn.setText("▶");
         } else {
-//            playSelectedSong();
+            playSelectedSong();
             play_btn.setText("⏸");
         }
         isPlaying = !isPlaying;
     }
 
-//    private void playSelectedSong() {
-//        try {
-//            stop(); // Detener la reproducción actual antes de comenzar una nueva
-//            String songPath = "src/main/resources/com/group/reproductorjava/songs/" + songNameLabel.getText() + ".mp3";
-//            currentSongPath = songPath;
-//
-//            Media media = new Media(new File(songPath).toURI().toString());
-//            mediaPlayer = new MediaPlayer(media);
-//
-//            mediaPlayer.setOnEndOfMedia(() -> stop());
-//
-//            if (currentMediaTime != null) {
-//                mediaPlayer.seek(currentMediaTime);
-//            }
-//
-//            mediaPlayer.play();
-//            loadSelectedSong();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private void playSelectedSong() {
+        try {
+            stop(); // Detener la reproducción actual antes de comenzar una nueva
+            String songPath = "src/main/resources/com/group/reproductorjava/songs/" + songNameLabel.getText() + ".mp3";
+            currentSongPath = songPath;
+
+            Media media = new Media(new File(songPath).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+
+            mediaPlayer.setOnEndOfMedia(() -> stop());
+
+            if (currentMediaTime != null) {
+                mediaPlayer.seek(currentMediaTime);
+            }
+
+            mediaPlayer.play();
+            loadSelectedSong();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void pause() {
         if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
@@ -172,7 +172,7 @@ public class PlayerViewController {
      */
     @FXML
     private void goHome() throws IOException {
-//        stop();
+        stop();
         HelloApplication.setRoot("Home-view");
     }
 
@@ -186,7 +186,7 @@ public class PlayerViewController {
             songDurationLabel.setText(String.valueOf(currentSong.getDuration()));
             songGenderLabel.setText(currentSong.getGender());
             songRepLabel.setText(String.valueOf(currentSong.getnReproductions()));
-            songDiscLabel.setText(String.valueOf(currentSong.getDisco()));
+            songDiscLabel.setText(String.valueOf(currentSong.getDisco().getName()));
             try {
                 String imagePath = currentSong.getDisco().getPhoto();
                 InputStream inputStream = getClass().getResourceAsStream(imagePath);
